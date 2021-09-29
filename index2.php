26a55febe64c1b22dc90dc770cc7905e2732243e
msimen <msimen@users.noreply.github.com>
2021-09-29 15:19:25 +0100

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,7 +1,7 @@
 <?php
 require_once '/var/www/html/vendor/autoload.php';
 use Moometric\mooSignature;
-$mooSig = new mooSignature("gmail.com", "msimens@gmail.com");
+$mooSig = new mooSignature("msimens@gmail.com");
 // Optionally set the path where your default service-account.json file is stored.
 $mooSig->addSettingServiceAccountPath("/home/ubuntu/local_vars/");
 // Optionally set the path where your signatures are stored.
